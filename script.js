bf9d05a814592c1f1c43dcd72b9ddaf25be260ad
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2022-09-24 00:34:12 +0600

    simple calculator done

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ function getFormattedNumber(num){
     n = n.toLocaleString('en');
     return n;
 }
+function calculate(previous, current){
+    var result = previous + current;
+    result = eval(result);
+    return result;
+}
 
 var operator = document.getElementsByClassName('operator');
 for (let i = 0; i < operator.length; i++) {
@@ -29,6 +34,83 @@ for (let i = 0; i < operator.length; i++) {
             var current = getOutput();
             var newCurrent = current.slice(0, -1);
             printOutput(newCurrent);
+        }
+        else if(operator[i].id=='+'){
+            var previous = getHistory();
+            var current = getOutput();
+            if(current == '') {}
+            else{
+                if(previous ==''){
+                    current = current+ '+';
+                    printHistory(current);
+                    printOutput('');
+                }
+                else{
+                    let result = calculate(previous, current);
+                    printHistory(result + '+');
+                    printOutput('');
+                }
+            }
+        }
+        else if(operator[i].id=='-'){
+            var previous = getHistory();
+            var current = getOutput();
+            if(previous ==''){
+                current = current+ '-';
+                printHistory(current);
+                printOutput('');
+            }
+            else if(current ==''){
+                current = current+ '-';
+                printOutput('-');
+            }
+            else{
+                let result = calculate(previous, current);
+                printHistory(result + '-');
+                printOutput('');
+            }
+        }
+        else if(operator[i].id=='*'){
+            var previous = getHistory();
+            var current = getOutput();
+            if(current == '') {}
+            else{
+                if(previous ==''){
+                    current = current+ '*';
+                    printHistory(current);
+                    printOutput('');
+                }
+                else{
+                    let result = calculate(previous, current);
+                    printHistory(result + '*');
+                    printOutput('');
+                }
+            }
+        }
+        else if(operator[i].id=='/'){
+            var previous = getHistory();
+            var current = getOutput();
+            if(current == '') {}
+            else{
+                if(previous ==''){
+                    current = current+ '/';
+                    printHistory(current);
+                    printOutput('');
+                }
+                else{
+                    var current = getOutput();
+                    let result = calculate(previous, current);
+                    printHistory(result + '/');
+                    printOutput('');
+                }
+            }
+        }
+        else if(operator[i].id=='='){
+            var previous = getHistory();
+            var current = getOutput();
+            var result = calculate(previous, current);
+            printHistory('');
+            printOutput(result);
         }
     });
 }
